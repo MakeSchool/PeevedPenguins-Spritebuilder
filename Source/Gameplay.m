4533b128f1bf35f79dd006ccce386f6ff0dd5c7a
Ben-G <Benjamin.Encz@gmail.com>
2014-01-16 10:01:49 -0800

    neat level loading in place

--- Source/Gameplay.m
+++ Source/Gameplay.m
@@ -9,11 +9,29 @@
 #import "Gameplay.h"
 
 @implementation Gameplay {
-    CCNode* _levelNode;
+    CCNode *_levelNode;
+    CCNode *_catapultArm;
+    CCPhysicsNode *_physicsNode;
 }
 
 - (void)didLoadFromCCB {
     [_levelNode addChild:[CCBReader load:@"Levels/Level1"]];
+}
+
+- (void)launchPenguin {
+    CCNode* penguin = [CCBReader load:@"Penguin"];
+    penguin.position = ccpAdd(_catapultArm.position, ccp(40, 115));
+    
+    [_physicsNode addChild:penguin];
+    
+    CGPoint launchDirection = ccp(1, 0);
+    CGPoint force = ccpMult(launchDirection, 8000);
+    
+    [penguin.physicsBody applyForce:force];
+    
+    float screenWidth = [[CCDirector sharedDirector] viewSize].width;
+    CCAction* scrollScreen = [CCActionMoveTo actionWithDuration:2.f position:ccp(screenWidth-self.contentSize.width, self.position.y)];
+    [self runAction:scrollScreen];
 }
 
 @end
