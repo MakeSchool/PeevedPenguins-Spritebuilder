0599071dc17fcf302b1f796edae9dbefab4a43ed
Ben-G <Benjamin.Encz@gmail.com>
2014-01-29 09:18:04 -0800

    fixed level names in code

--- Source/Gameplay.m
+++ Source/Gameplay.m
@@ -48,7 +48,7 @@ static const float MIN_SPEED = 5.f;
     self.userInteractionEnabled = TRUE;
     
     // load a level
-    CCScene *level = [CCBReader loadAsScene:@"levels/level1"];
+    CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
     [_levelNode addChild:level];
     
     // visualize physic bodies & joints
